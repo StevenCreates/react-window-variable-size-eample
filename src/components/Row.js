a875bf2324888e1a9321e20f890c4279e5e064ce
StevenCreates <steven@stevencreates.tech>
2021-07-28 20:05:49 -0600

    framework

---
+++ src/components/Row.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Row = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/resources/RowData.js
@@ -0,0 +1,62 @@
+const rows = [
+    {
+        date: '01/21/2021',
+        firstName: 'Steven',
+        lastName: 'Creates',
+        search: 'Data Vizualization'
+    },
+    {
+        date: '09/24/2022',
+        firstName: 'Levi',
+        lastName: 'McCormick',
+        search: 'Design'
+    },
+    {
+        date: '11/01/2020',
+        firstName: 'Brendon',
+        lastName: 'Hyde',
+        search: 'Funds'
+    },
+    {
+        date: '04/03/2018',
+        firstName: 'Raptor',
+        lastName: 'Hofheins',
+        search: 'Extreme Sports'
+    },
+    {
+        date: '04/03/2018',
+        firstName: 'Zephyr',
+        lastName: 'Hofheins',
+        search: 'Dates'
+    },
+    {
+        date: '01/21/2021',
+        firstName: 'Steven',
+        lastName: 'Creates',
+        search: 'Figma'
+    },
+    {
+        date: '09/24/2022',
+        firstName: 'Levi',
+        lastName: 'McCormick',
+        search: 'Front End React'
+    },
+    {
+        date: '11/01/2020',
+        firstName: 'Brendon',
+        lastName: 'Hyde',
+        search: 'Big Bank'
+    },
+    {
+        date: '04/03/2018',
+        firstName: 'Raptor',
+        lastName: 'Hofheins',
+        search: 'Skiing'
+    },
+    {
+        date: '04/03/2018',
+        firstName: 'Zephyr',
+        lastName: 'Hofheins',
+        search: 'Doll House'
+    },
+]

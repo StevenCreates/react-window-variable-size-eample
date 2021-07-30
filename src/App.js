09f77c4b6d110b90676f75d265be65bc711c2685
StevenCreates <steven@stevencreates.tech>
2021-07-29 22:11:55 -0600

    added app styling

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
-import { FixedSizeList as List } from 'react-window';
-import {rows} from './resources/RowData';
+import { FixedSizeList as List } from "react-window";
+import { rows } from "./resources/RowData";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  background: #313a3a;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
 
 function App() {
-
-
   const Row = (props) => {
-    const {index, data, style} = props
-    return (
-      <div style={style}>
-        {data[index].firstName}
-      </div>
-    )
-  }
-  console.log(rows)
+    const { index, data, style } = props;
+    return <div style={style}>{data[index].firstName}</div>;
+  };
+
+  console.log(rows);
+
   return (
-  <>
-  
-  
-  
-   <List
-   height={150}
-   itemCount={rows.length}
-   itemSize={50}
-   itemData={rows}
-   width={300}
- >
-   {Row}
- </List>
-  </>
+    <AppContainer>
+      <List
+        height={300}
+        itemCount={rows.length}
+        itemSize={50}
+        itemData={rows}
+        width={300}
+      >
+        {Row}
+      </List>
+    </AppContainer>
   );
 }
 
